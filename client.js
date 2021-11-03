1244acdcdffe06bec43182ccbb380558c680d9c5
Ammar <ammar.kusow2@gmail.com>
2021-11-03 15:03:59 -0400

    second commiter

--- client.js
+++ client.js
@@ -3,12 +3,17 @@ const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
-  });
+  
+});
   conn.setEncoding("utf8");
+conn.on('connect', ()=>{
+console.log('connected')
+conn.write("Name: AMA")
+})
 
   return conn;
 };
 
 
-
-  module.exports = {connect}
+exports.connect = connect;
+  //module.exports = {connect}
